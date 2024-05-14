ef48871a2b8abc59ff62532a234a99084b6f2d95
Juliana Michelsen <juju.michelsen@gmail.com>
2024-05-14 07:53:08 -0300

    ajustando formulas e adicionando fatores

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import sqlite3
 
 app = FastAPI()
 
-numero_EmissaoAgua = 175
-numero_EmissaoCarne = 175
-numero_EmissaoEnergia = 175
+NUMERO_EMISSAO_AGUA = 175
+NUMERO_EMISSAO_CARNE = 175
+NUMERO_EMISSAO_ENERGIA = 175
+NUMERO_EMISSAO_COMBUSTIVEL = 175
 
 class RequestData(BaseModel):
-    numero_vAgua: float
-    numero_cCarne: float
-    numero_cEnergia: float
+    numero_consumo_agua: float
+    numero_consumo_carne: float
+    numero_consumo_energia: float
+    numero_consumo_gasolina: float
+    numero_consumo_alcool: float
 
 class Resultado(BaseModel):
     id: int
@@ -19,23 +22,35 @@ class Resultado(BaseModel):
     num2: float
     resultado: float
 
-
 @app.post("/calcular/")
 async def calcular_numeros(item: RequestData):
+    if item.numero_consumo_agua <=0:
+        return "Não foi possível realizar a operação. O consumo de água deve ser zero ou positivo."
+    if item.numero_consumo_carne <0:
+        return "Não foi possível realizar a operação. O consumo de carne dever ser zero ou positivo."
+    if item.numero_consumo_energia <0:
+        return "Não foi possível realizar a operação. O consumo de energia dever ser zero ou positivo."
+    if item.numero_consumo_gasolina <0:
+        return "Não foi possível realizar a operação. O consumo de gasolina dever ser zero ou positivo."
+    if item.numero_consumo_alcool <0:
+        return "Não foi possível realizar a operação. O consumo de alcool dever ser zero ou positivo."
     
-    if item.numero_vAgua <=0:
-        return "Não foi possível realizar a operação. Por favor, digite um número positivo."
+    fator_emissao_agua = NUMERO_EMISSAO_AGUA / item.numero_consumo_agua
     
-    if item.numero_cCarne <0:
-        return "Não foi possível realizar a operação. Por favor, digite um número positivo."
+    fator_emissao_carne = NUMERO_EMISSAO_CARNE / item.numero_consumo_carne
     
-    if item.numero_cEnergia <0:
-        return "Não foi possível realizar a operação. Por favor, digite um número positivo."
+    fator_emissao_energia = NUMERO_EMISSAO_ENERGIA / item.numero_consumo_energia
     
-    fator_EmissaoAgua = numero_EmissaoAgua / item.numero_vAgua
+    if item.numero_consumo_gasolina > 0:
+        fator_emissao_combustivel_gasolina = (NUMERO_EMISSAO_COMBUSTIVEL ** 2) / item.numero_consumo_gasolina
+    else:
+        fator_emissao_combustivel_gasolina = 0
     
-    fator_EmissaoCarne = numero_EmissaoCarne / item.numero_cCarne
-    
-    fator_EmissaoEnergia = numero_EmissaoEnergia / item.numero_cEnergia
+    if item.numero_consumo_alcool > 0:
+        fator_emissao_combustivel_alcool = (NUMERO_EMISSAO_COMBUSTIVEL ** 2) / item.numero_consumo_alcool
+    else:
+        fator_emissao_combustivel_alcool = 0
+        
+    total_emissao = fator_emissao_agua + fator_emissao_carne + fator_emissao_energia + fator_emissao_combustivel_gasolina + fator_emissao_combustivel_alcool
 
-    return "Emissão Água:" + str(fator_EmissaoAgua) + " Emissão Carne:" + str(fator_EmissaoCarne) + " Emissão Energia:" + str(fator_EmissaoEnergia)
+    return {"Emissão Água" :  fator_emissao_agua, "Emissão Carne" :  fator_emissao_carne, "Emissão Energia" : fator_emissao_energia, "Emissão Gasolina" : fator_emissao_combustivel_gasolina, "Emissão Álcool" : fator_emissao_combustivel_alcool, "TotalEmissao" : total_emissao}
